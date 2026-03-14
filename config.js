e0113092ca4dad6d197fe00e943ba04dc5436918
Jenbin1125 <wangjenbin@gmail.com>
2026-03-14 23:47:19 +0800

    部署修正：上傳 config.js 供 Zeabur 讀取

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js (這個檔案不要上傳到 GitHub)
+window.ENV = {
+    SUPABASE_URL: 'https://mydityyenvjktldmvqui.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15ZGl0eXllbnZqa3RsZG12cXVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NTAyODEsImV4cCI6MjA4NzMyNjI4MX0.-LyWO1em4ZHPcE_jYAxegzBjhtgJ82E1VOi7ZRKRrYg'
+};
